1cc2e5a383d2c93c2fe3ba553f8e27da85ecfcf3
Stephan Viljoen 11008408 <u11008408@tuks.co.za>
2014-06-28 22:19:16 +0200

    Software Structure
    The structure used, according to the specifications of Entelect

---
+++ Web_CA/Data_Access/src/main/java/Data_Access/App.java
@@ -0,0 +1,13 @@
+package Data_Access;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
