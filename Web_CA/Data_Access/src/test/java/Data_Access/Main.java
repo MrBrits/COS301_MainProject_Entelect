d7787ca2f36196a6a3394f3bc5e12a87687d6bb4
Christo <christo.brits92@gmail.com>
2014-07-02 15:40:31 +0200

    Hibernate is working now

--- Web_CA/Data_Access/src/test/java/Data_Access/Main.java
+++ Web_CA/Data_Access/src/test/java/Data_Access/Main.java
@@ -3,6 +3,8 @@ package Data_Access;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.Session;
+import za.co.tera.Data_Access.impl.Direction;
+import za.co.tera.Data_Access.impl.State;
 import za.co.tera.Data_Access.impl.User;
 
 public class Main {
@@ -10,9 +12,6 @@ public class Main {
     //private static final Logger LOGGER = LoggerFactory.getLogger(Web_ca.class);
 
     public static void main(final String[] args) throws Exception {
-
-
-
         Configuration configuration = new Configuration();
         configuration.configure();
 
@@ -20,17 +19,24 @@ public class Main {
         Session session= sessionFactory.openSession();
         session.beginTransaction();
 
+        State state = new State();
+        state.setOwnerId(18);
+        state.setStateDesc("gravity");
+        state.setStateName("Gravity");
+        state.setStateRgb("12345");
+        state.setStateValue(1.1);
         User user = new User();
         user.setUserEmail("@gmail");
         user.setUserFirstName("John");
         user.setUserLastName("Doe");
-        user.setUserPassword(12356);
+        user.setUserPassword("1234");
 
-        session.save(user);
+
+        Direction direct = new Direction();
+        direct.setDirectionName("up");
+
+        //session.save(direct);
+        session.save(state);
         session.getTransaction().commit();
-
-
-
-
     }
 }
