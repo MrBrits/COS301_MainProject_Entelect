7c3ca60aa41ae0bc6fc0757f0878b4ac1858a713
Stephan Viljoen 11008408 <u11008408@tuks.co.za>
2014-07-15 14:38:36 +0200

    rulescript-changed

--- Web_CA/Presentation/src/main/webapp/resources/js/ruleScript.js
+++ Web_CA/Presentation/src/main/webapp/resources/js/ruleScript.js
@@ -1,3 +1,6 @@
+/**
+ * Created by Laptop on 7/15/2014.
+ */
 var web_ca = angular.module('rule_app', []);
 
 web_ca.controller("RuleGetCtr", function ($scope, $http) {
@@ -14,14 +17,14 @@ web_ca.controller("RuleGetCtr", function ($scope, $http) {
 });
 web_ca.controller("RuleSetCtr", function ($scope, $http) {
     var app = this;
-/*
-    $http.get("http://localhost:8080/getAllRules")
-        .success(function (data) {
-            //alert(data);
-            $scope.rules = data;
+    /*
+     $http.get("http://localhost:8080/getAllRules")
+     .success(function (data) {
+     //alert(data);
+     $scope.rules = data;
 
-        }).error(function () {
-            alert("fail");
-        });*/
+     }).error(function () {
+     alert("fail");
+     });*/
     alert("Ddf");
 });
